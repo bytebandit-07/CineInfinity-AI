71e2a60ef3620ea5c3a9b69abe60ecae1686d0ef
Shadow <ashirbinhamid96@gmail.com>
2025-05-09 09:08:24 +0500

    making a prototype for main menu

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,18 +1,25 @@
 import tkinter as tk
+import customtkinter as ctk
 
 def search_movie():
     movie = entry.get()
     result_label.config(text=f"Searching for recommendations based on: {movie}")
 
-root = tk.Tk()
-root.title("CineInfinity AI - Movie Recommender")
+root = ctk.CTk()
+ctk.set_appearance_mode("dark")
+root.geometry("900x600")
+root.title("Film Flow")
 
-tk.Label(root, text="Enter Movie Name:").pack()
+menu_frame=ctk.CTkFrame(root,fg_color="#E50914",width=1400,height=50,corner_radius=False)
+menu_frame.place(x=1,y=1)
+
+
+tk.Label(root, text="Enter Movie Name:").pack(pady=400)
 entry = tk.Entry(root)
-entry.pack()
+entry.pack(pady=450)
 
-tk.Button(root, text="Get Recommendations", command=search_movie).pack()
+tk.Button(root, text="Get Recommendations", command=search_movie).pack(pady=600)
 result_label = tk.Label(root, text="")
-result_label.pack()
+result_label.pack(pady=620)
 
 root.mainloop()

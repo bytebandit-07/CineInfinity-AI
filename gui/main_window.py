2a0eb57cd6bd423b4b2ef1dedf26a5801b8ef4aa
Shadow <ashirbinhamid96@gmail.com>
2025-05-09 09:52:21 +0500

    added search bar

--- gui/main_window.py
+++ gui/main_window.py
@@ -13,6 +13,12 @@ root.title("Film Flow")
 menu_frame=ctk.CTkFrame(root,fg_color="#E50914",width=1400,height=50,corner_radius=False)
 menu_frame.place(x=1,y=1)
 
+title_label=ctk.CTkLabel(menu_frame,text="Film Flow",text_color="white",font=("morganite",22,"bold")).place(x=12,y=10)
+
+search_bar=ctk.CTkEntry(menu_frame,placeholder_text="Search",placeholder_text_color="black",width=500,height=30,fg_color="#f0f0f0",
+                        text_color="black",border_width=0,corner_radius=150,justify="center").place(x=400,y=10)
+
+
 
 tk.Label(root, text="Enter Movie Name:").pack(pady=400)
 entry = tk.Entry(root)

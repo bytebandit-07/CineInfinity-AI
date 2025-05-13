3874596a77e9e52c1490e10ac3e4e5e65726b176
Talal <muhammadtalal780@gmail.com>
2025-05-13 20:24:31 +0500

    Removed database folder from Git and added to .gitignore

--- database/db_connector.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def connect_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="your_username",
-        password="your_password",
-        database="cine_db"
-    )

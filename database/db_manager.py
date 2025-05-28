f9d13eca1c25b9a6758648119b8451794ab1a497
Talha Aamir <talhaaamir4990@gmail.com>
2025-05-29 02:00:02 +0500

    updated database functions

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,24 +1,11 @@
 # db_manager.py
 
 import mysql.connector
+from db_config import get_connection
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-# Establish connection
-conn = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
-)
+conn = get_connection()
 cursor = conn.cursor()
 
 # Add a new user
